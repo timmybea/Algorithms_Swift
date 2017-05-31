c7ad1e90b62bf6b161eadb808ecf3fd469c7518d
Tim Beals <tim.beals@gmail.com>
2017-05-31 10:45:03 -0700

    create optimized pair matching algorithm using dictionary to track values and indices

--- Udemy_Intro_To_Algorithms/3_The_Power_of_Algorithms/3_11_Pair_Matching.playground/Contents.swift
+++ Udemy_Intro_To_Algorithms/3_The_Power_of_Algorithms/3_11_Pair_Matching.playground/Contents.swift
@@ -2,6 +2,11 @@
 
 import UIKit
 
+//Pair Matching: The challenge is to find two elements in an array that, when added together equal is the same as out target value.
+//eg: array: [1,2,2,3,4] target: 4 will yield the indices (0, 3) and (1, 2)
+
+
+//MARK: Setup methods
 public class BenchTimer {
     
     static func measureBlock(closure:() -> Void) -> TimeInterval {
@@ -30,8 +35,8 @@ public extension CFTimeInterval {
     }
 }
 
-//Pair Matching: The challenge is to find two elements in an array that, when added together equal is the same as out target value.
-//eg: array: [1,2,2,3,4] target: 4 will yield the indices (0, 3) and (1, 2)
+
+//MARK: Brute-force approach
 
 func findTwoSum(_ array: [Int], target: Int) -> (Int, Int)? {
     
@@ -51,18 +56,155 @@ func findTwoSum(_ array: [Int], target: Int) -> (Int, Int)? {
     return nil
 }
 
-
 let array = [1,2,2,3,4]
 if let indices = findTwoSum(array, target: 4) {
-    print(indices)
+    //print(indices)
 } else {
-    print("No pairs found")
+    //print("No pairs found")
 }
 
 //This initial approach will use quadratic time, as you can see by the nested for loops. Specifically: (n^2 + n) / 2. Here is how we work it out.
-
 //The outer loop iterates through the entire array of n iterations
-//
+
+
+
+
+
+//MARK: Optimized function
+//In this optimized version we can perform the check with linear complexity because we are storing the values and indices in a dictionary as we traverse the array. Checking the to see if the right value is a key in the dictionary is a constant time because it is a hash function.
+
+func optimizedFindTwoSum(_ array: [Int], target: Int) -> (Int, Int)? {
+    
+    guard array.count > 1 else {
+        return nil
+    }
+    
+    var diffs = Dictionary<Int, Int>()
+
+    for i in 0..<array.count {
+        
+        let left = array[i]
+        
+        let right = target - left
+        
+        if let foundIndex = diffs[right] {
+            return (i, foundIndex)
+        } else {
+            diffs[left] = i
+        }
+    }
+    return nil
+}
+
+//MARK: Test!
+
+//create random array
+var size = 10
+var target = 4
+var maxValue: UInt32 = 4
+
+func generateArray(size: Int, maxValue: UInt32) -> [Int] {
+    var array = Array(repeating: 0, count: size)
+    for i in 0..<size {
+        array[i] = Int(arc4random_uniform(maxValue))
+    }
+    return array
+}
+
+var testArray = generateArray(size: size, maxValue: maxValue)
+
+
+var executionTime = BenchTimer.measureBlock {
+    _ = findTwoSum(testArray, target: Int(maxValue))
+}
+print("findTwoSum: for size = \(size), execution time = \(executionTime.formattedTime)")
+
+
+executionTime = BenchTimer.measureBlock {
+    _ = optimizedFindTwoSum(testArray, target: Int(maxValue))
+}
+
+print("OptimizedFindTwoSum: for size = \(size), execution time = \(executionTime.formattedTime)")
+
+//For size 50
+size = 50
+target = 10
+maxValue = 10
+
+testArray = generateArray(size: size, maxValue: maxValue)
+
+executionTime = BenchTimer.measureBlock {
+    _ = findTwoSum(testArray, target: Int(maxValue))
+}
+print("findTwoSum: for size = \(size), execution time = \(executionTime.formattedTime)")
+
+
+executionTime = BenchTimer.measureBlock {
+    _ = optimizedFindTwoSum(testArray, target: Int(maxValue))
+}
+
+print("OptimizedFindTwoSum: for size = \(size), execution time = \(executionTime.formattedTime)")
+
+
+//for size 100
+size = 100
+target = 50
+maxValue = 50
+
+testArray = generateArray(size: size, maxValue: maxValue)
+
+executionTime = BenchTimer.measureBlock {
+    _ = findTwoSum(testArray, target: Int(maxValue))
+}
+print("findTwoSum: for size = \(size), execution time = \(executionTime.formattedTime)")
+
+
+executionTime = BenchTimer.measureBlock {
+    _ = optimizedFindTwoSum(testArray, target: Int(maxValue))
+}
+
+print("OptimizedFindTwoSum: for size = \(size), execution time = \(executionTime.formattedTime)")
+
+
+//for size 200
+size = 200
+target = 10
+maxValue = 100
+
+testArray = generateArray(size: size, maxValue: maxValue)
+
+executionTime = BenchTimer.measureBlock {
+    _ = findTwoSum(testArray, target: Int(maxValue))
+}
+print("findTwoSum: for size = \(size), execution time = \(executionTime.formattedTime)")
+
+
+executionTime = BenchTimer.measureBlock {
+    _ = optimizedFindTwoSum(testArray, target: Int(maxValue))
+}
+
+print("OptimizedFindTwoSum: for size = \(size), execution time = \(executionTime.formattedTime)")
+
+
+//for size 500
+size = 500
+target = 100
+maxValue = 60
+
+testArray = generateArray(size: size, maxValue: maxValue)
+
+executionTime = BenchTimer.measureBlock {
+    _ = findTwoSum(testArray, target: Int(maxValue))
+}
+print("findTwoSum: for size = \(size), execution time = \(executionTime.formattedTime)")
+
+
+executionTime = BenchTimer.measureBlock {
+    _ = optimizedFindTwoSum(testArray, target: Int(maxValue))
+}
+
+print("OptimizedFindTwoSum: for size = \(size), execution time = \(executionTime.formattedTime)")
+
 
 
 
